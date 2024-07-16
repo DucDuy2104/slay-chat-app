008a1680c6202720c5533c6ac5064ec63a56d0f4
DucDuy2104 <117710024+DucDuy2104@users.noreply.github.com>
2024-07-16 08:38:55 +0700

    update code

---
+++ src/page/Wellcome/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Wellcome = () => {
+  return (
+    <View>
+      <Text>Wellcome</Text>
+    </View>
+  )
+}
+
+export default Wellcome
---
+++ src/page/Wellcome/style.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+const wellcomeStyle = StyleSheet.create({
+
+})
+
+export default wellcomeStyle;
