6b35177af9e042ec04345df449073d1bfd9ebf76
azthan888 <pkgamingthanh@gmail.com>
2024-07-17 01:52:30 +0700

    Thêm icon + code giao diện profile

--- src/page/Profile/index.js
+++ src/page/Profile/index.js
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import profileStyle from './style'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={profileStyle.container}>
+      <View style={profileStyle.profileContainer}>
+        <Image
+          style={profileStyle.profileImage}
+          source={require('../../assets/image/avatar.jpg')}
+        />
+        <View style={profileStyle.profileInfo}>
+          <Text style={profileStyle.profileName}>Đào Minh Thành</Text>
+          <Text style={profileStyle.profileEmail}>dmthanh@gmail.com</Text>
+        </View>
+      </View>
+
+      <TouchableOpacity style={profileStyle.button} onPress={() => alert('Cài đặt')}>
+      <Image
+          style={profileStyle.icon}
+          source={require('./setting.png')}
+        />
+        <Text style={profileStyle.buttonText}>Cài đặt</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={profileStyle.button} onPress={() => alert('Gửi phản hồi')}>
+      <Image
+          style={profileStyle.icon}
+          source={require('./feedback.png')}
+        />
+        <Text style={profileStyle.buttonText}>Gửi phản hồi</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[profileStyle.button, profileStyle.logoutButton]} onPress={() => alert('Đăng xuất')}>
+      <Image
+          style={profileStyle.icon}
+          source={require('./logout.png')}
+        />
+        <Text style={[profileStyle.buttonText, profileStyle.logoutButtonText]}>Đăng xuất</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/page/Profile/style.js
+++ src/page/Profile/style.js
@@ -1,7 +1,58 @@
 import { StyleSheet } from "react-native";
 
 const profileStyle = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        backgroundColor: '#a0e9e1',
+        padding: 16,
+      },
+      profileContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 10,
+        marginBottom: 16,
+      },
+      profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 16,
+      },
+      profileInfo: {
+        flexDirection: 'column',
+      },
+      profileName: {
+        fontSize: 16,
+        fontWeight: 'bold',
+      },
+      profileEmail: {
+        fontSize: 14,
+        color: 'gray',
+      },
+      button: {
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        padding: 16,
+        borderRadius: 10,
+        marginBottom: 16,
+        alignItems: 'center',
+      },
+      buttonText: {
+        fontSize: 16,
+        marginLeft: 10,
+      },
+      icon: {
+        width: 24,
+        height: 24,
+      },
+      logoutButton: {
+        backgroundColor: 'white',
+      },
+      logoutButtonText: {
+        color: 'red',
+      }
 })
 
 export default profileStyle;
