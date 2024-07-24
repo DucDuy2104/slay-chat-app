4d9274485ca9490bea398f4879663fb59dd2496a
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-07-24 20:56:02 +0700

    Dinh Nhan push code Forgot Pass

--- src/page/ForgotPass/index.js
+++ src/page/ForgotPass/index.js
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableHighlight, TextInput } from 'react-native'
 import React from 'react'
-
+import forgotPassStyle  from './style'
 const ForgotPass = () => {
   return (
-    <View>
-      <Text>ForgotPass</Text>
+    <View style={forgotPassStyle.container}>
+    <View style={forgotPassStyle.header}>
+      <Image style={forgotPassStyle.image} source={require('../../assets/image/backright.png')} />
+      <Text style={forgotPassStyle.textHeader}>Sửa thông tin</Text>
+      <Text/>
     </View>
+    <View>
+      <Text style={forgotPassStyle.text}>Nhập mật khẩu</Text>
+      <TextInput placeholder='Nhập mật khẩu cũ' style={forgotPassStyle.input} />
+      <Text style={forgotPassStyle.text}>Nhập mật khẩu</Text>
+      <TextInput placeholder='Nhập mật khẩu mới' style={forgotPassStyle.input} />
+      <Text style={forgotPassStyle.text}>Xác nhận mật khẩu</Text>
+      <TextInput placeholder='Nhập mật khẩu' style={forgotPassStyle.input} />
+      <TouchableHighlight style={forgotPassStyle.button}>
+        <Text style={forgotPassStyle.buttonText}>Cập nhật</Text>
+      </TouchableHighlight>
+    </View>
+  </View>
   )
 }
 
--- src/page/ForgotPass/style.js
+++ src/page/ForgotPass/style.js
@@ -1,4 +1,71 @@
 import { StyleSheet } from "react-native";
 const forgotPassStyle = StyleSheet.create({
-    
+    container: {  
+        flex: 1,
+        backgroundColor: '#A7F2D7',
+      },
+      header: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        paddingVertical: 16
+      },
+      image: {
+        width: 23,
+        height: 23
+      },
+      avatarContainer: {
+        marginVertical: 50,
+        alignItems: 'center',
+        width: '100%',
+        justifyContent: 'center'
+      },
+      add: {
+        width: 24,
+        height: 24,
+        position: 'absolute',
+        top: 155,
+        right: 120
+      },
+      avatar: {
+        marginTop: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      textHeader: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#000000'
+      },
+      text: {
+        marginLeft: 20,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#000000',
+        marginTop: 24
+      },
+      input: {
+        margin: 20,
+        borderColor: '#000000',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 13,
+        marginTop: 8
+      },
+      button: {
+        backgroundColor: '#41C8E5',
+        padding: 13,
+        borderRadius: 10,
+        margin: 20
+      },
+      buttonText: {
+        color: '#000000',
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold'
+      }
 })
+
+export default forgotPassStyle
