3d0b708f9d152a5617f8893601de43978e255907
DinhNhanFpoly <hailuazzz2004@gmail.com>
2024-07-24 19:15:34 +0700

    Update MainStack.js

--- src/routes/MainStack.js
+++ src/routes/MainStack.js
@@ -14,7 +14,7 @@ const MainStack = () => {
      screenOptions={{
       headerShown: false
      }}
-     initialRouteName='wellcome'
+     initialRouteName='Login'
     >
       <Stack.Screen name="Wellcome" component={Wellcome} />
       <Stack.Screen name="Login" component={Login}/>
